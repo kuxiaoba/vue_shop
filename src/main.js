fff34c8fe509546b3afed1d23939baf1c067d221
喻宁保 <5128401+yu0510@user.noreply.gitee.com>
2019-07-06 21:28:45 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-//导入字体图标
+// 手动配置elementui
+import ElementUI from 'element-ui'
+import ' element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+    //导入字体图标
 import './assets/fonts/iconfont.css'
 //导入全局样式
 import './assets/css/global.css'
@@ -16,6 +20,6 @@ Vue.config.productionTip = false
 
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
